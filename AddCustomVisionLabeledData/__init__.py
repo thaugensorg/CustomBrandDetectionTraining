d72fe8bb854191939ad807b071ad6fc718054660
toddhaugen <todd@haugen.name>
2019-08-06 17:29:26 -0700

    code complete custom training model

--- AddCustomVisionLabeledData/__init__.py
+++ AddCustomVisionLabeledData/__init__.py
@@ -1,93 +1,50 @@
 import logging
 import time
+import os
 
 import azure.functions as func
 
 from azure.cognitiveservices.vision.customvision.training import CustomVisionTrainingClient
-from azure.cognitiveservices.vision.customvision.training.models import ImageFileCreateEntr
+from azure.cognitiveservices.vision.customvision.training.models import ImageFileCreateEntry
 from azure.cognitiveservices.vision.customvision.prediction import CustomVisionPredictionClient
+
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    name = req.params.get('name')
-    if not name:
+    BlobUrl = req.params.get('blobUrl')
+    Labels = req.params.get('labels')
+    if not BlobUrl:
         try:
             req_body = req.get_json()
         except ValueError:
             pass
         else:
-            name = req_body.get('name')
+            BlobUrl = req_body.get('blobUrl')
 
-    if name:
+    if BlobUrl:
 
         # https://pypi.org/project/custom_vision_client/
+        # https://github.com/Azure-Samples/cognitive-services-python-sdk-samples/blob/master/samples/vision/custom_vision_training_samples.py
 
         ENDPOINT = "https://westus2.api.cognitive.microsoft.com"
 
         # Get Cognitive Services Environment Variables
-        training_key = os.environ['trainingKey']
-        prediction_key = os.environ['predictionKey']
-        prediction_resource_id = os.environ['predictionID']
+        TrainingKey = os.environ['trainingkey']
 
-        publish_iteration_name = "prototypeModel"
+        Trainer = CustomVisionTrainingClient(TrainingKey, endpoint=ENDPOINT)
 
-        trainer = CustomVisionTrainingClient(training_key, endpoint=ENDPOINT)
-
-        project = trainer.create_project("CustomBrandDetection")
-
-        # Make two tags in the new project
-        hemlock_tag = trainer.create_tag(project.id, "Hemlock")
-        cherry_tag = trainer.create_tag(project.id, "Japanese Cherry")
-
-        base_image_url = "<path to project>"
+        Project = Trainer.create_project("CustomBrandDetection")
 
         print("Adding images...")
 
-        image_list = []
-
         # Set image_url to the URL of the image that will be analyzed
-        image_url = name
+        ImageUrl = BlobUrl
 
-        for image_num in range(1, 11):
-            file_name = "hemlock_{}.jpg".format(image_num)
-            with open(base_image_url + "images/Hemlock/" + file_name, "rb") as image_contents:
-                image_list.append(ImageFileCreateEntry(name=file_name, contents=image_contents.read(), tag_ids=[hemlock_tag.id]))
+        # ImageFileCreateEntry(name=file_name, contents=image_contents.read(), tag_ids=[hemlock_tag.id])
 
-        for image_num in range(1, 11):
-            file_name = "japanese_cherry_{}.jpg".format(image_num)
-            with open(base_image_url + "images/Japanese Cherry/" + file_name, "rb") as image_contents:
-                image_list.append(ImageFileCreateEntry(name=file_name, contents=image_contents.read(), tag_ids=[cherry_tag.id]))
-
-        upload_result = trainer.create_images_from_urls()
-        upload_result = trainer.create_images_from_files(project.id, images=image_list)
-        if not upload_result.is_batch_successful:
-            print("Image batch upload failed.")
-            for image in upload_result.images:
-                print("Image status: ", image.status)
-            exit(-1)
-
-        print ("Training...")
-        iteration = trainer.train_project(project.id)
-        while (iteration.status != "Completed"):
-            iteration = trainer.get_iteration(project.id, iteration.id)
-            print ("Training status: " + iteration.status)
-            time.sleep(1)
-
-        # The iteration is now trained. Publish it to the project endpoint
-        trainer.publish_iteration(project.id, iteration.id, publish_iteration_name, prediction_resource_id)
-        print ("Done!")
-
-        # Now there is a trained endpoint that can be used to make a prediction
-        predictor = CustomVisionPredictionClient(prediction_key, endpoint=ENDPOINT)
-
-        with open(base_image_url + "images/Test/test_image.jpg", "rb") as image_contents:
-            results = predictor.classify_image(
-                project.id, publish_iteration_name, image_contents.read())
-
-        # Display the results.
-        for prediction in results.predictions:
-            print("\t" + prediction.tag_name + ": {0:.2f}%".format(prediction.probability * 100))
+        upload_result = Trainer.create_images_from_urls(Project.project_id, ImageUrl, tag_ids=[Labels])
+        # upload_result = Trainer.create_images_from_files(Project.id, images=image_list)
 
     else:
         return func.HttpResponse(
--- LoadImageTags/__init__.py
+++ LoadImageTags/__init__.py
@@ -1,8 +1,12 @@
 import logging
 import requests
 import json
-
+import os
 import azure.functions as func
+
+from azure.cognitiveservices.vision.customvision.training import CustomVisionTrainingClient
+# from azure.cognitiveservices.vision.customvision.training.models import ImageFileCreateEntr
+from azure.cognitiveservices.vision.customvision.prediction import CustomVisionPredictionClient
 
 # https://2.python-requests.org//en/latest/api/
 # https://stackoverflow.com/questions/9746303/how-do-i-send-a-post-request-as-a-json
@@ -23,10 +27,14 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         except ValueError:
             pass
         else:
+            ENDPOINT = "https://westus2.api.cognitive.microsoft.com"
+            TrainingKey = os.environ['trainingkey']
+            Trainer = CustomVisionTrainingClient(TrainingKey, endpoint=ENDPOINT)
+
             LabelDictionary = json.loads(req_body)
-            for (i in LabelDictionary.Labels) {
-                Label = trainer.create_tag(project.id, LabelDictionary.Labels[i])
-            }
+            for i in LabelDictionary.values():
+                Label = Trainer.create_tag(ProjectID, LabelDictionary.values()[i])
+                
             return func.HttpResponse("Loaded " + i + " labels into " + ProjectID)
     else:
         return func.HttpResponse(
