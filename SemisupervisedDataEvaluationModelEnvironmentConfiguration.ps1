d1902d60bc7276a30f59b64d05d84224c3574c45
toddhaugen <todd@haugen.name>
2019-08-17 23:26:27 -0700

    config script fixes

--- SemisupervisedDataEvaluationModelEnvironmentConfiguration.ps1
+++ SemisupervisedDataEvaluationModelEnvironmentConfiguration.ps1
@@ -3,27 +3,44 @@
 while([string]::IsNullOrWhiteSpace($subscription))
   {$subscription= Read-Host -Prompt "Input the name of the subscription where this solution will be deployed"}
 
-$modelResourceGroupName = Read-Host -Prompt 'Input the name of the resource group that you want to create for this installation of the model.  (default=SemisupervisedModel)'
-  if ([string]::IsNullOrWhiteSpace($modelResourceGroupName)) {$modelResourceGroupName = "SemisupervisedModel"}
+$modelLocation = Read-Host -Prompt 'Input the Azure location, data center, where you want this solution deployed.  Note, if you will be using Python functions as part of your solution, As of 8/1/19, Python functions are only available in eastasia, eastus, northcentralus, northeurope, westeurope, and westus.  If you deploy your solution in a different data center network transit time may affect your solution performance.  (default=westus)'
+if ([string]::IsNullOrWhiteSpace($modelLocation)) {$modelLocation = "westus"}
+
+$modelResourceGroupName = Read-Host -Prompt 'Input the name of the resource group that you want to create for this installation of the model.  (default=SemisupervisedDataScienceModel)'
+  if ([string]::IsNullOrWhiteSpace($modelResourceGroupName)) {$modelResourceGroupName = "SemisupervisedDataScienceModel"}
   
-while([string]::IsNullOrWhiteSpace($SemisupervisedAppName))
-  {$SemisupervisedAppName= Read-Host -Prompt "Input the name for the azure function app you want to create for your analysis model. Note this must be a name that is unique across all of Azure"}
+while([string]::IsNullOrWhiteSpace($ModelAppName))
+  {$ModelAppName= Read-Host -Prompt "Input the name for the azure function app you want to create for your analysis model. Note this must be a name that is unique across all of Azure"}
 
 while([string]::IsNullOrWhiteSpace($modelStorageAccountName))
-  {$modelStorageAccountName= Read-Host -Prompt "Input the name of the azure storage account you want to create for this installation of the model. Note this must be a name that only uses lowercase letters and numbers and is unique across all of Azure"}
+  {$modelStorageAccountName= Read-Host -Prompt "Input the name of the azure storage account you want to create for this installation of the model. Note this must be a name that is no longer than 24 characters and only uses lowercase letters and numbers and is unique across all of Azure"
+  if ($modelStorageAccountName.length -gt 24){$modelStorageAccountName=$null
+  Write-Host "Storage account name cannot be longer than 24 charaters." -ForegroundColor "Red"}
+  if ($modelStorageAccountName -cmatch '[A-Z]') {$modelStorageAccountName=$null
+  Write-Host "Storage account name must not have upper case letters." -ForegroundColor "Red"}
+  }
+
+$cognitiveServicesAccountName = Read-Host -Prompt 'Input the name of the resource group that you want to create for this installation of the model.  (default=ImageDetection)'
+  if ([string]::IsNullOrWhiteSpace($cognitiveServicesAccountName)) {$cognitiveServicesAccountName = "ImageDetection"}
+  
+Write-Host "Storage Account Name: " $modelStorageAccountName -ForegroundColor "Green"
 
 $modelStorageAccountKey = $null
-$modelLocation = "westus"
 
 if (az group exists --name $modelResourceGroupName) `
-	{az group delete `
+  {Write-Host "Deleting resource group." -ForegroundColor "Green" `
+  az group delete `
 	  --name $modelResourceGroupName `
 	  --subscription $subscription `
 	  --yes -y}
 
+Write-Host "Creating Resource Group: " $modelResourceGroupName  -ForegroundColor "Green"
+
 az group create `
   --name $modelResourceGroupName `
   --location $modelLocation 
+
+Write-Host "Creating storage account: " $modelStorageAccountName  -ForegroundColor "Green"
 
 az storage account create `
     --location $modelLocation `
@@ -31,18 +48,24 @@ az storage account create `
     --resource-group $modelResourceGroupName `
     --sku Standard_LRS
 
+Write-Host "Getting storage account key." -ForegroundColor "Green"
+
 $modelStorageAccountKey = `
 	(get-azureRmStorageAccountKey `
 		-resourceGroupName $modelResourceGroupName `
 		-AccountName $modelStorageAccountName).Value[0]
 
+Write-Host "Creating function app: " $ModelAppName -ForegroundColor "Green"
+
 az functionapp create `
-  --name $brandDetectionAppName `
+  --name $ModelAppName `
   --storage-account $modelStorageAccountName `
   --consumption-plan-location $modelLocation `
   --resource-group $modelResourceGroupName `
   --os-type "Linux" `
   --runtime "python"
+
+Write-Host "Creating cognitive services account." -ForegroundColor "Green"
 
 az cognitiveservices account create `
     --name "brandDetection" `
@@ -51,10 +74,28 @@ az cognitiveservices account create `
     --sku F0 `
     --location westus `
     --yes
+    
+az cognitiveservices account create `
+    --name "ImageDetectionTraining" `
+    --resource-group $modelResourceGroupName `
+    --kind CustomVision.Training `
+    --sku F0 `
+    --location "westus2" `
+    --yes
+
+az cognitiveservices account create `
+    --name "ImageDetectionPrediction" `
+    --resource-group $modelResourceGroupName `
+    --kind CustomVision.Prediction `
+    --sku F0 `
+    --location "westus2" `
+    --yes
+
+Write-Host "Creating app config setting: subscriptionKey" -ForegroundColor "Green"
 
 az functionapp config appsettings set `
-    --name $brandDetectionAppName `
-    --resource-group imageAnalysisModel `
+    --name $ModelAppName `
+    --resource-group $modelResourceGroupName `
     --settings "subscriptionKey=Null"
 
 #gitrepo=https://github.com/thaugensorg/semi-supervisedModelSolution.git
